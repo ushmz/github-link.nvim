94e80a025f8ea11d80a83cb27ae386842bc90344
ushmz <44394399+ushmz@users.noreply.github.com>
2023-12-03 16:10:30 +0900

    feat: show error if no commit exists on the remote

--- lua/github-link/init.lua
+++ lua/github-link/init.lua
@@ -6,22 +6,33 @@ local function get_file_name()
 	return vim.fn.expand("%")
 end
 
+---@class Error
+---@field message string
+
 ---Get the ref of the current file
 ---@param which_ref string The ref type, one of `branch`, `head`, `file`
----@return string | nil ref The ref name or nil if error
+---@return string ref The ref name
+---@return Error | nil err The error message or nil if no error
 local function get_ref(which_ref)
+  local ref
 	if which_ref == "branch" then
-		local ref = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
-		return ref
+		ref = vim.fn.systemlist("git rev-parse --abbrev-ref HEAD")[1]
+		local exist = vim.fn.systemlist("git ls-remote --exit-code --heads origin " .. ref)[1]
+		if not exist then
+			return "", { message = "Remote branch does not exist" }
+		end
 	elseif which_ref == "head" then
-		local ref = vim.fn.systemlist("git rev-parse HEAD")[1]
-		return ref
+		ref = vim.fn.systemlist("git rev-parse HEAD")[1]
 	elseif which_ref == "file" then
-		local ref = vim.fn.systemlist("git rev-list -1 HEAD -- " .. vim.fn.shellescape(vim.fn.expand("%")))[1]
-		return ref
+		ref = vim.fn.systemlist("git rev-list -1 HEAD -- " .. vim.fn.shellescape(vim.fn.expand("%")))[1]
+		if not ref then
+			return "", { message = "No commits yet" }
+		end
 	else
-		return nil
+		return "", { message = "Unknown ref type: " .. which_ref }
 	end
+
+	return ref, nil
 end
 
 ---Get the link to the current file in the current branch
@@ -102,6 +113,7 @@ end
 
 local M = {}
 
+---@param which_ref "branch" | "head" | "file" The ref type, one of `branch`, `head`, `file`
 function M.get_commit_link(which_ref)
 	local remote = vim.fn.systemlist("git remote get-url origin")[1]
 	if not string.match(remote, ".*github.*") then
@@ -115,9 +127,9 @@ function M.get_commit_link(which_ref)
 		return
 	end
 
-	local ref = get_ref(which_ref)
-	if not ref then
-		vim.api.nvim_err_writeln("[github-link.nvim] Unknown ref type: " .. which_ref)
+	local ref, err = get_ref(which_ref)
+	if err then
+		vim.api.nvim_err_writeln("[github-link.nvim] " .. err.message)
 		return
 	end
 
@@ -132,6 +144,7 @@ function M.get_commit_link(which_ref)
 	vim.print(link)
 end
 
+---@param which_ref "branch" | "head" | "file" The ref type, one of `branch`, `head`, `file`
 function M.get_commit_link_range(which_ref)
 	local remote = vim.fn.systemlist("git remote get-url origin")[1]
 	if not string.match(remote, ".*github.*") then
@@ -145,9 +158,9 @@ function M.get_commit_link_range(which_ref)
 		return
 	end
 
-	local ref = get_ref(which_ref)
-	if not ref then
-		vim.api.nvim_err_writeln("[github-link.nvim] Unknown ref type: " .. which_ref)
+	local ref, err = get_ref(which_ref)
+	if err then
+		vim.api.nvim_err_writeln("[github-link.nvim] " .. err.message)
 		return
 	end
 
